08ea05745bb7ca72b59af28b95b07612f3aae91b
dwichan0905 <52048022+dwichan0905@users.noreply.github.com>
2020-06-22 11:41:54 +0700

    Modul Background Task

---
+++ BackgroundTask/app/src/main/java/id/dwichan/backgroundtask/MainActivity.kt
@@ -0,0 +1,83 @@
+package id.dwichan.backgroundtask
+
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.ref.WeakReference
+
+class MainActivity : AppCompatActivity(), MyAsyncCallback {
+
+    companion object {
+        private const val INPUT_STRING = "Hai dari AsyncTask!"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val demoAsync = DemoAsync(this)
+        demoAsync.execute(INPUT_STRING)
+    }
+
+    override fun onPreExecute() {
+        tvStatus.text = getString(R.string.status_pre)
+        tvDesc.text = INPUT_STRING
+    }
+
+    override fun onPostExecute(result: String) {
+        tvStatus.text = getString(R.string.status_post)
+        tvDesc.text = result
+    }
+
+    private class DemoAsync(val myListener: MyAsyncCallback): AsyncTask<String, Void, String>() {
+        companion object {
+            private val LOG_ASYNC = "DemoAsync"
+        }
+
+        private val myListener2: WeakReference<MyAsyncCallback>
+
+        init {
+            this.myListener2 = WeakReference(myListener)
+        }
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            Log.d(LOG_ASYNC, "status: onPreExecute()")
+
+            val myListener = myListener2.get()
+            myListener?.onPreExecute()
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+
+            Log.d(LOG_ASYNC, "status: onPostExecute()")
+
+            val myListener = this.myListener2.get()
+            myListener?.onPostExecute(result.toString())
+        }
+
+        override fun doInBackground(vararg params: String?): String {
+            Log.d(LOG_ASYNC, "status: doInBackground()")
+
+            var output: String? = null
+
+            try {
+                val input = params[0]
+                output = "$input Selamat belajar!"
+                Thread.sleep(2000)
+            } catch (e: Exception) {
+                Log.d(LOG_ASYNC, e.message)
+            }
+
+            return output.toString()
+        }
+    }
+}
+
+internal interface MyAsyncCallback {
+    fun onPreExecute()
+    fun onPostExecute(text: String)
+}
