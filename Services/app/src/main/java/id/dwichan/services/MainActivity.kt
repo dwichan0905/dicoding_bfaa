ac84cec4d337e9ce24f35f9422f540a4a30d0849
dwichan0905 <52048022+dwichan0905@users.noreply.github.com>
2020-06-22 14:28:33 +0700

    Modul Service: Bound Service

--- Services/app/src/main/java/id/dwichan/services/MainActivity.kt
+++ Services/app/src/main/java/id/dwichan/services/MainActivity.kt
@@ -1,12 +1,33 @@
 package id.dwichan.services
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var mServiceBound = false
+    private lateinit var mBoundService: MyBoundService
+
+    private val mServiceConnection = object: ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName?) {
+            mServiceBound = false
+        }
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val myBinder = service as MyBoundService.MyBinder
+            mBoundService = myBinder.getService
+            mServiceBound = true
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,12 +52,18 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
 
             R.id.btnStartBoundService -> {
-
+                val mBoundServiceIntent = Intent(this@MainActivity, MyBoundService::class.java)
+                bindService(mBoundServiceIntent, mServiceConnection, Context.BIND_AUTO_CREATE)
             }
 
             R.id.btnStopBoundService -> {
-
+                unbindService(mServiceConnection)
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mServiceBound) unbindService(mServiceConnection)
     }
 }
