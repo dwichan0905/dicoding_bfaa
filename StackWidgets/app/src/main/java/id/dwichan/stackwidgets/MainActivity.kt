beb77ea16d55fe4bb7a4a9d6f1cd040383863d75
dwichan0905 <52048022+dwichan0905@users.noreply.github.com>
2020-07-08 01:26:16 +0700

    Modul Widget: StackView Widget

---
+++ StackWidgets/app/src/main/java/id/dwichan/stackwidgets/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.dwichan.stackwidgets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
