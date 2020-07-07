f31bf2ae3be5120f687ca24b66c3be0a0b9b98a2
dwichan0905 <52048022+dwichan0905@users.noreply.github.com>
2020-07-08 01:54:27 +0700

    Modul Notifikasi: PendingIntent

--- SimpleNotification/app/src/main/java/id/dwichan/simplenotification/MainActivity.kt
+++ SimpleNotification/app/src/main/java/id/dwichan/simplenotification/MainActivity.kt
@@ -2,11 +2,15 @@ package id.dwichan.simplenotification
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
@@ -20,9 +24,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendNotification(view: View) {
+        // PendingIntent: untuk persiapan apabila notifikasi di klik
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://fb.com/CdrScNET89")) // <-- target nya kemana?
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
 
         val mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentIntent(pendingIntent) // PendingIntent diterapkan disini
                 .setSmallIcon(R.drawable.ic_baseline_notifications_24) // ini bakal muncul di bagian notification bar, icon kecil sebelum judul notif (kalo ada)
                 .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_baseline_notifications_24))
                 .setContentTitle(resources.getString(R.string.content_title))
